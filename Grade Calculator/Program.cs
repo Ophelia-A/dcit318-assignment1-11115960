8047b4a56ec9f9f76f451fc4a170f08507889824
Ophelia Awo Ayisi <151508923+Ophelia-A@users.noreply.github.com>
2025-08-05 00:11:28 -0200

    Completed Grade Calculator with input validation and retry loop

---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    int grade;
+    while (true)
+        {
+            Console.Write("Enter your grade (0–100): ");
+            bool isValid = int.TryParse(Console.ReadLine(), out grade);
+
+            if (!isValid || grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Your input is invalid. Please enter a number between 0 and 100.\n");
+            }
+            else
+            {
+                break; // valid input, exit loop
+            }
+        
+        
+        }
+
+        // Determine and print letter grade
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your letter grade is: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your letter grade is: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your letter grade is: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your letter grade is: D");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is: F");
+        }
+    }
+}
