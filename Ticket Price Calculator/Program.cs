4c3180aa9e5ae82cd0641ae2f7a58bd029ae235d
Ophelia Awo Ayisi <151508923+Ophelia-A@users.noreply.github.com>
2025-08-05 00:26:07 -0200

    Added ticket price calculator

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age;
+
+        while (true)
+        {
+            Console.Write("How old are you? ");
+            if (int.TryParse(Console.ReadLine(), out age) && age >= 0)
+                break;
+
+            Console.WriteLine("Invalid input. Please enter a valid non-negative whole number.\n");
+        }
+
+        int ticketPrice;
+
+        if (age <= 12 || age >= 65)
+        {
+            ticketPrice = 7;
+        }
+        else
+        {
+            ticketPrice = 10;
+        }
+
+        Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+    }
+}
